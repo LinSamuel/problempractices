dd8d25035e2b4d74d4694a04a963a266537f1f9e
LinSamuel <samueltanlin@gmail.com>
2017-01-04 14:58:51 -0800

    challenge

---
+++ src/problempractices/Anagram.java
@@ -0,0 +1,47 @@
+package problempractices;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Anagram {
+
+    public static int numberNeeded(String first, String second) {
+        HashMap<String, Integer> firstMap = new HashMap<String,Integer>();
+        //HashMap<String, Integer> secondMap = new HashMap<String, Integer>();
+        
+        for(char letter : first.toCharArray()){
+        	if (firstMap.containsKey(String.valueOf(letter))){
+        		firstMap.put(String.valueOf(letter), firstMap.get(String.valueOf(letter)) + 1);
+        	} else {
+        		firstMap.put(String.valueOf(letter), 1);
+        	}
+        }
+        for(char letter : second.toCharArray()){
+        	if (firstMap.containsKey(String.valueOf(letter))){
+        		firstMap.put(String.valueOf(letter), firstMap.get(String.valueOf(letter)) - 1);
+        	} else {
+        		firstMap.put(String.valueOf(letter), -1);
+        	}
+        }
+        Iterator it = firstMap.entrySet().iterator();
+        int minMoves = 0;
+        while (it.hasNext()){
+        	Map.Entry pair = (Map.Entry)it.next();
+        	minMoves += Math.abs((int) pair.getValue());
+        }
+        
+        
+    	return minMoves;
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+        
+    }
+
+}
--- src/problempractices/LeftRotateArray.java
+++ src/problempractices/LeftRotateArray.java
@@ -4,35 +4,35 @@ import java.util.Scanner;
 
 public class LeftRotateArray {
 
-		public static void main(String[] args) {
-	        Scanner in = new Scanner(System.in);
-	        int n = in.nextInt();
-	        int k = in.nextInt();
-	        int a[] = new int[n];
-	        for(int a_i=0; a_i < n; a_i++){
-	            a[a_i] = in.nextInt();
-	        }
-	        String answer = new String();
+//		public static void main(String[] args) {
+//	        Scanner in = new Scanner(System.in);
+//	        int n = in.nextInt();
+//	        int k = in.nextInt();
+//	        int a[] = new int[n];
+//	        for(int a_i=0; a_i < n; a_i++){
+//	            a[a_i] = in.nextInt();
+//	        }
+//	        String answer = new String();
+////	        for(int i = k; i < a.length; i++){
+////	        	answer += String.valueOf(a[i]);
+////	        }
+////	        for(int i = 0; i < k; i++){
+////	        	answer += String.valueOf(a[i]);
+////	        }
+//	        if (k > n){
+//	        	k = k % n;
+//	        }
+//	        StringBuilder theAnswer = new StringBuilder();
+//	        //StringBuilder finalAnswer = new StringBuilder();
 //	        for(int i = k; i < a.length; i++){
-//	        	answer += String.valueOf(a[i]);
+//	        	theAnswer.append(a[i]);
+//	        	theAnswer.append(" ");
 //	        }
 //	        for(int i = 0; i < k; i++){
-//	        	answer += String.valueOf(a[i]);
+//	        	theAnswer.append(a[i]);
+//	        	theAnswer.append(" ");
 //	        }
-	        if (k > n){
-	        	k = k % n;
-	        }
-	        StringBuilder theAnswer = new StringBuilder();
-	        //StringBuilder finalAnswer = new StringBuilder();
-	        for(int i = k; i < a.length; i++){
-	        	theAnswer.append(a[i]);
-	        	theAnswer.append(" ");
-	        }
-	        for(int i = 0; i < k; i++){
-	        	theAnswer.append(a[i]);
-	        	theAnswer.append(" ");
-	        }
-
-	        System.out.print(theAnswer.toString().trim());
-	}
+//
+//	        System.out.print(theAnswer.toString().trim());
+//	}
 }
